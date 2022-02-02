e6982c0deb1173c56a1590117ae1d9b83f91c640
Florian Legendre <flegendre.pro@gmail.com>
2022-02-02 21:10:08 +0100

    Added TestLoader class. All test parameters from input files seem to be correctly loaded

--- codes/src/simulator/Main.java
+++ codes/src/simulator/Main.java
@@ -3,23 +3,12 @@ package simulator;
 import inputParsers.Dependencies;
 import inputParsers.Jobs;
 import inputParsers.Servers;
+import inputParsers.TestLoader;
 
 public class Main {
 
     public static void main(String[] args) {
-        //Testing Jobs files reading:
-	    Jobs jobs = new Jobs("../res/inputs/input1_ut3/jobs.txt");
-        jobs.print();
-        System.out.println();
-
-        //Testing Servers file reading:
-        Servers servers = new Servers("../res/inputs/input1_ut3/servers.txt");
-        servers.print();
-        System.out.println();
-
-        //Testing Dependency file reading:
-        Dependencies dependencies = new Dependencies("../res/inputs/input1_ut3/dependencies.txt");
-        dependencies.print();
-        System.out.println();
+        TestLoader test = new TestLoader("../res/inputs/input1_ut3/", "test1.txt");
+        test.print();
     }
 }
