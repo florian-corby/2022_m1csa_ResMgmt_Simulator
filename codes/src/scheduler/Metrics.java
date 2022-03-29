7e69b234ae9584e520ef0b0d7b360d3f40eed3de
Florian Legendre <flegendre.pro@gmail.com>
2022-03-29 19:50:06 +0200

    feat: Added new statistics in Metrics class
    Metrics class now displays average tardiness/consumption + servers
    workload + consumption per server.

--- codes/src/scheduler/Metrics.java
+++ codes/src/scheduler/Metrics.java
@@ -16,6 +16,7 @@ public class Metrics {
     private HashMap<Job, Double> tardMap = new HashMap<>();
     private double[] totalConsumption;
     private double[][] serversConsumption;
+    private double[] serversWorkTime;
     private Schedule schedule;
 
     /* ============== CONSTRUCTORS ============== */
@@ -27,11 +28,18 @@ public class Metrics {
         serversConsumption = new double[nbServers][simulationTime];
         setTardiness();
         setConsumption();
+        setServersWorkTime();
     }
 
     /* ============== GETTERS ============== */
+    public double getAverageConsumption(){ return getTotalConsumption() / nbServers; }
+    public double getAverageTardiness(){
+        double res = 0;
+        for(double t : tardMap.values()) res += t;
+        return tardMap.size() == 0 ? 0 : res / tardMap.entrySet().size();
+    }
     public Set<Job> getLateJobs(){ return tardMap.keySet(); }
-    public double getMaxConsumption(){ double res = 0; for(double v : totalConsumption) if(v > res) res = v; return res;}
+    public double getMaxConsumption(){ double res = 0; for(double v : totalConsumption) if(v > res) res = v; return res; }
     public double getMaxTardiness(){
         return tardMap.entrySet().size() == 0 ? 0 : Collections.max(tardMap.entrySet(), HashMap.Entry.comparingByValue()).getValue();
     }
@@ -43,6 +51,12 @@ public class Metrics {
                 res = entry.getServer().getId();
         return res+1;
     }
+    public double getServerConsumption(int idxServer){
+        double res = 0;
+        for(double v : serversConsumption[idxServer]) res += v;
+        return res;
+    }
+    public double getTotalConsumption() { double res = 0; for(double v : totalConsumption) res += v; return res; }
     public double getTotalMakespan(){ return schedule.getLastEntry().getEnd(); }
 
     /* ============== SETTERS ============== */
@@ -56,7 +70,11 @@ public class Metrics {
                 }
         }
     }
-
+    public void setServersWorkTime(){
+        serversWorkTime = new double[nbServers];
+        for(ScheduleEntry entry : schedule.getAllEntries())
+            serversWorkTime[entry.getServer().getId()] += (entry.getEnd() - entry.getStart());
+    }
     public void setTardiness(){
         for (ScheduleEntry entry: schedule.getAllEntries()) {
             if(entry.getJob().getADeadline() < entry.getEnd()) {
@@ -68,14 +86,27 @@ public class Metrics {
 
     /* ============== PRINTERS ============== */
     public void print(){
-        System.out.println("################ SCHEDULE METRICS ################\n");
-        System.out.println("> Total Makespan: " + getTotalMakespan());
-        System.out.println("> Nb Deadline Misses: " + getNbDeadlineMisses());
-        System.out.println("> Max Tardiness: " + getMaxTardiness());
-        System.out.println("> Late Jobs: ");
-        for(Map.Entry<Job, Double> entry : tardMap.entrySet()){ System.out.println("[Job: " + entry.getKey()
+        System.out.println("################ SCHEDULE METRICS ################");
+        System.out.println("\n>> Scheduling Metrics: ");
+        System.out.println("- Total Makespan: " + getTotalMakespan());
+        System.out.println("- Nb Deadline Misses: " + getNbDeadlineMisses());
+        System.out.println("- Max Tardiness: " + getMaxTardiness());
+        System.out.println("- Average Tardiness: " + getAverageTardiness());
+        System.out.println("- Late Jobs: ");
+        for(Map.Entry<Job, Double> entry : tardMap.entrySet()){ System.out.println("\t[Job: " + entry.getKey()
                                                                 + " | Tardiness: " + entry.getValue() + " ]"); }
-        System.out.println("> Max Consumption: " + getMaxConsumption());
+
+        System.out.println("\n>> Servers Metrics: ");
+        System.out.println("- Servers work load:");
+        for(int i = 0; i < nbServers; i++) System.out.println("\tServer #" + i + " : " + serversWorkTime[i]);
+
+        System.out.println("\n>> Energy Metrics: ");
+        System.out.println("- Total Consumption: " + getTotalConsumption());
+        System.out.println("- Max Consumption: " + getMaxConsumption());
+        System.out.println("- Average Consumption: " + getAverageConsumption());
+        System.out.println("- Consumption per Server: ");
+        for(int i = 0; i < serversConsumption.length; i++)
+            System.out.println("\tServer #" + i + " : " + getServerConsumption(i));
         //for(int i = 0; i < consumption.length; i++) System.out.println("[Time: " + i + " | Consumption: " + consumption[i] + " ]");
         System.out.println("\n##################################################\n");
     }
